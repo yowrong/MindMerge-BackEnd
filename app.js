e35a72381112830cfa9f2c0f31fb4a504a38a334
Andy Chang <ychang118@my.bcit.ca>
2021-12-17 18:53:56 -0800

    node js server with socketio

---
+++ app.js
@@ -0,0 +1,25 @@
+const port = process.env.PORT || 3000;
+const app = require("express") ();
+const http = require("http").createServer(app);
+const socketio = require("socket.io") (http);
+// const WebSocket = require("ws");
+const msg = "Welcome to Mind Merge!";
+
+app.get("/", (req, res) => {
+    res.send(msg);
+})
+
+// const server = new WebSocket.Server(
+//     {
+//       port: port,
+//     }
+// );
+
+socketio.on("connection", (userSocket) => {
+    userSocket.on("send_message", (data) => {
+        userSocket.broadcast.emit("receive_message", data);
+    })
+})
+
+http.listen(port);
+
