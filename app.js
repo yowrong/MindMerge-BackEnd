dafd8c3f5ad4b08cb299e46fdb124d879c3b8f37
Andy Chang <ychang118@my.bcit.ca>
2021-12-18 19:50:05 -0800

    add socket calls

--- app.js
+++ app.js
@@ -19,11 +19,28 @@ app.get("/", (req, res) => {
 //     }
 // );
 
+let users = [];
+
+const userJoin = (id, username) => {
+    const user = { id, username };
+    users.push(user);
+    return user;
+}
+
 socketio.on("connection", (userSocket) => {
     userSocket.emit("welcome", {message: "Donna is the best <3"});
     userSocket.on("send_message", (data) => {
         userSocket.broadcast.emit("receive_message", data);
     });
+    userSocket.on("createRoom", () => {
+        userSocket.emit("initRoom", {roomCode: "ABCD"});
+    });
+    userSocket.on("joinRoom", (data) => {
+        if (data == "ABCD") {
+            const user = userJoin(userSocket.id, data);
+        }
+        userSocket.emit("confirmRoom", {roomCode: "ABCD"});
+    })
 })
 
 http.listen(port);
