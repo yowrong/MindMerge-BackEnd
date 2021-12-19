4e33b8567d1205041addd22d83a34ec1872e706f
Kevin <kevinyuanderchang@gmail.com>
2021-12-19 01:39:54 -0800

    socket.io funtionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const port = process.env.PORT || 3000;
 const app = require("express")();
 const http = require("http").createServer(app);
 const socketio = require("socket.io")(http);
+const game = require("game.js")(io)
 const msg = "Welcome to Mind Merge!";
 
 app.get("/", (req, res) => {
@@ -28,11 +29,18 @@ socketio.on("connection", (userSocket) => {
         userSocket.emit("initRoom", {roomCode: "ABCD", players: users});
         userSocket.emit("newPlayer", {roomCode: "ABCD", players: users});
     });
+
     userSocket.on("joinRoom", (data) => {
         userJoin(userSocket.id, data);
         userSocket.emit("confirmRoom", {roomCode: "ABCD", players: users});
         userSocket.emit("newPlayer", {roomCode: "ABCD", players: users});
     })
+    
+    var socket = socketio.connect('http://localhost');
+    socket.emit('game.js-event');
+
 })
 
-http.listen(port);
+http.listen(port, function () {
+    console.log('Listening on port ' + port + '!');
+});
